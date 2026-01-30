352e1846c4f04f86fe8b5dceb77b172fa254afd3
Mário Melo <mariomelo@users.noreply.github.com>
2026-01-30 17:18:12 +0100

    fix: adicionar permissões corretas para plausible_user no Postgres

--- stacks/shared/postgres/init-scripts/create-databases.sh
+++ stacks/shared/postgres/init-scripts/create-databases.sh
@@ -10,7 +10,19 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 
     -- Criar usuário específico para Plausible
     CREATE USER plausible_user WITH PASSWORD '${PLAUSIBLE_DB_PASSWORD}';
-    GRANT ALL PRIVILEGES ON DATABASE plausible TO plausible_user;
+
+    -- Dar ownership do banco ao usuário
+    ALTER DATABASE plausible OWNER TO plausible_user;
+
+    -- Conectar ao banco plausible e dar permissões no schema public
+    \c plausible
+    GRANT ALL PRIVILEGES ON SCHEMA public TO plausible_user;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO plausible_user;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO plausible_user;
+
+    -- Garantir que futuras tabelas também tenham permissão
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO plausible_user;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO plausible_user;
 
     -- Futuras databases podem ser adicionadas aqui
     -- CREATE DATABASE outra_app;
